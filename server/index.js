6c9ab7c53b43eccf560cd5c507eccdec157e2d75
engsanjid <mdsanjidi36@gmail.com>
2026-04-25 02:29:45 +0600

    upd

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://bshpranto.vercel.app"],
+  origin: ["http://localhost:5173",
+    "https://copy-submanager.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
